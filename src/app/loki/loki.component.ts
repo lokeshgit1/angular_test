03ef57365f95de5a724ae94e8d34316c85a99f7a
lokeshgit1 <chlokeshreddy.2.0@gmail.com>
2025-11-03 11:08:33 +0530

    added components added router

---
+++ src/app/loki/loki.component.ts
@@ -0,0 +1,63 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loki',
+  templateUrl: './loki.component.html',
+  styleUrls: ['./loki.component.css']
+})
+export class LokiComponent {
+
+  isDarkMode = false;
+
+  formData = {
+    name: '',
+    phone: '',
+    flat: '',
+    city: ''
+  };
+  
+  colorName: string = "red";
+  message: string = "";
+  msg: string = "";
+  localPeople :boolean = true;
+
+  toggleMode() {
+    this.isDarkMode = !this.isDarkMode;
+  }
+  displaySubmitButton : boolean = false;
+
+  handleSubmit() {
+    console.log('Form Submitted:', this.formData);
+    alert(`Address submitted successfully for ${this.formData.name}!`);
+    
+    this.msg = `Full Name: ${this.formData.name}
+    Phone Number: ${this.formData.phone}
+    Flat/House No: ${this.formData.flat}
+    City: ${this.formData.city}
+    
+    ✅ Your address is submitted successfully!`;
+    
+   // 👇 Reset the form fields
+  this.formData = {
+    name: '',
+    phone: '',
+    flat: '',
+    city: ''
+  };
+
+
+  }
+  toggleColor(){
+    this.colorName = this.colorName === "red" ? "blue" : "red";
+     // update message to show in frontend
+    this.message = 'Button is clicked';
+
+  }
+  studentData = [
+    {name: 'Alice', age: 30, grade: 'A'},
+    {name: 'Bob', age: 32, grade: 'B'},
+    {name: 'Charlie', age: 41, grade: 'A'},
+    {name: 'David', age: 23, grade: 'C'},
+    {name: 'Eva', age: 60, grade: 'B'}
+  ]
+}
